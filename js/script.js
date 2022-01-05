99f423e10d4eda8316258db8361f58767ea28039
silos-sk <sil21019@byui.edu>
2022-01-05 22:46:41 +0000

    update js to querySelector

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let date = new Date();
 let year = date.getFullYear();
 
-document.getElementById("year").textContent = year;
+document.querySelector("#year").textContent = year;
 
 document.getElementById("lu").textContent =
   "Last Updated: " + document.lastModified;
