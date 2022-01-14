6a2f74d6eaa2a81d718389cc1467a9346f455a8d
silos-sk <sil21019@byui.edu>
2022-01-14 00:02:23 +0000

    design principles files

---
+++ lesson2/js/script.js
@@ -0,0 +1,7 @@
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector("#year").textContent = year;
+
+document.getElementById("lu").textContent =
+  " Last Updated: " + document.lastModified;
