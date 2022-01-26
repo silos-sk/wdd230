48aa76c8a62dd036cb4beeb3b99eb4ba0ab1cc93
silos-sk <sil21019@byui.edu>
2022-01-26 23:21:21 +0000

    chamber files

---
+++ chamber/js/script.js
@@ -0,0 +1,49 @@
+let completeDate = new Date();
+const daysName = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const monthName = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+// Header Date display
+const day = daysName[completeDate.getDay()];
+const todaysDate = completeDate.getDate();
+const month = monthName[completeDate.getMonth()];
+const year = completeDate.getFullYear();
+
+const currentDate = `${day}, ${todaysDate} ${month} ${year}`;
+
+document.querySelector("#currentDate").textContent = currentDate;
+
+// Footer date display
+document.querySelector("#year").textContent = year;
+
+document.getElementById("lu").textContent =
+  " Last Updated: " + document.lastModified;
+
+// Hamburger Nav
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
