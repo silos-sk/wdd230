c0bde157f15d1eb6da4b8c5967ac1168d8ffd886
silos-sk <sil21019@byui.edu>
2022-02-09 20:36:15 +0000

    windchill html and js

---
+++ chamber/js/windchill.js
@@ -0,0 +1,17 @@
+const tempNum = parseFloat(document.querySelector("#temp").textContent);
+
+const speedNum = parseFloat(document.querySelector("#speed").textContent);
+
+let windChill =
+  35.74 +
+  0.6215 * tempNum -
+  35.75 * Math.pow(speedNum, 0.16) +
+  0.4275 * tempNum * Math.pow(speedNum, 0.16);
+
+windChill = Math.round(windChill);
+
+if (tempNum <= 50 && speedNum > 3) {
+  document.querySelector("#chill").textContent = `${windChill} \xB0F`;
+} else {
+  document.querySelector("#chill").textContent = "N/A";
+}
