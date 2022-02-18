84ea02f5891c9c97c24167f635cb7ff160763f51
silos-sk <sil21019@byui.edu>
2022-02-18 13:58:45 +0000

    discover page

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -57,3 +57,68 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+// DISCOVER PAGE //
+
+// Last visit
+const lvMessage = document.querySelector("#lastVisit");
+const lv = localStorage.getItem("lastVisit");
+const now = Date.now();
+
+console.log(`Last visit: ${lv}`);
+console.log(`Today: ${now}`);
+//1000 milliseconds in 1 second, 60 seconds in 1 minute, 60 minutes in an hour, 24 hours in 1 day.
+const msInDay = 1000 * 60 * 60 * 24;
+console.log(`milliseconds in a day is ${msInDay}`);
+
+let difference = Math.round((now - lv) / msInDay);
+
+console.log(difference);
+
+localStorage.setItem("lastVisit", Date.now());
+
+if (lv == null) {
+  lvMessage.textContent = `This is your first visit!`;
+} else {
+  lvMessage.textContent = `Last visit to this page: ${difference} days.`;
+}
+
+// LAZY LOAD IMAGES
+
+// get all imgs with data-src attribute
+const imagesToLoad = document.querySelectorAll("[data-src]");
+
+// optional parameters being set or the IntersectionalObserver
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px",
+};
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+// first check to see if Intersection Observer is supported
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+
+  // Loop through each img and check status of load if necessary
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  // just load ALL images if not supported
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
