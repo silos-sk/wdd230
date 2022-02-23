7da4ae97682a1c7991b214b1db2ba6ae7f47fc6b
silos-sk <sil21019@byui.edu>
2022-02-23 17:34:30 +0000

    join date and time

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -123,4 +123,11 @@ if ("IntersectionObserver" in window) {
   });
 }
 
+// JOIN PAGE CURRENT DATE AND TIME
+const joinDate = document.querySelector("#joinDate");
+joinDate.textContent = currentDate;
 
+const time = new Date().toLocaleTimeString();
+
+const joinTime = document.querySelector("#joinTime");
+joinTime.textContent = time;
