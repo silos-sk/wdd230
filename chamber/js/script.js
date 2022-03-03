30498db8c12556bd517d1002d995e9c3f6a7df85
silos-sk <sil21019@byui.edu>
2022-03-03 22:27:40 +0000

    directory list grid view

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -133,7 +133,7 @@ const joinTime = document.querySelector("#joinTime");
 joinTime.textContent = time;
 
 // DIRECTORY
-const requestURL = "data.json";
+const requestURL = "data/data.json";
 
 fetch(requestURL)
   .then(function (response) {
@@ -150,23 +150,102 @@ function displayCompanies(company) {
   let card = document.createElement("section");
   let para1 = document.createElement("p");
   let para2 = document.createElement("p");
+  let para3 = document.createElement("p");
   let link = document.createElement("a");
   let img = document.createElement("img");
+  let dir = document.querySelector("#directory");
+  let classDir = document.querySelector(".dir");
+  let dir1 = document.querySelectorAll(".dir1");
+  let dir1Even = document.querySelectorAll(".dir1:nth-child(even)");
+  const vw = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  );
+
   // Change the textContent property of the elements to contain company info
-  para1.textContent = `${company.address}`;
-  para2.textContent = `${company.phone}`;
+  para1.textContent = `${company.name}`;
+  para2.textContent = `${company.address}`;
+  para3.textContent = `${company.phone}`;
   link.textContent = `${company.website}`;
   link.setAttribute("href", `http://${company.website}`);
   img.setAttribute("src", `${company.logo}`);
   img.setAttribute("alt", `${company.name}`);
+  
+  // Default view (grid)
+  function defaultView() {
+    // Add/append the section(card)
+    card.appendChild(img);
+    card.appendChild(para1);
+    para1.classList.add("hidden");
+    card.appendChild(para2);
+    card.appendChild(para3);
 
-  // Add/append the section(card)
-  card.appendChild(img);
-  card.appendChild(para1);
-  card.appendChild(para2);
-  card.appendChild(link);
-  card.classList.add("dir1");
+    card.appendChild(link);
+    card.classList.add("dir1");
 
-  // Add/append the existing HTML div with the cards class with the section(card)
-  document.querySelector("div.dir").appendChild(card);
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector("div.dir").appendChild(card);
+    if (vw >= 1024) {
+      classDir.classList.add("dir-lrg");
+    }
+  }
+
+  // Call defaultView function
+  defaultView();
+
+  // Grid and List View Layout Buttons + Event Listeners
+  const gridViewbtn = document.querySelector(".fa-table-cells");
+  const listViewbtn = document.querySelector(".fa-list");
+
+  listViewbtn.addEventListener("click", function () {
+    listView();
+  });
+  gridViewbtn.addEventListener("click", function () {
+    defaultView();
+    listViewRemove();
+  });
+
+  // List View changes
+  function listView() {
+    para1.classList.remove("hidden");
+    para1.style.fontWeight = "bold";
+    img.classList.add("hidden");
+
+    listViewbtn.classList.add("view-active");
+    gridViewbtn.classList.remove("view-active");
+
+    dir1Even.forEach(even => {
+      even.classList.add("list-view");
+      console.log(even);
+    });
+
+    card.style.border = 0;
+
+    if (vw >= 560) {
+      dir.classList.add("list-layout-med");
+      dir1.forEach(section => {
+        section.classList.add("list-col");
+      });
+      classDir.classList.remove("dir-lrg");
+    } 
+  }
+
+  // Back to default changes
+  function listViewRemove() {
+    para1.classList.add("hidden");
+    img.classList.remove("hidden");
+    listViewbtn.classList.remove("view-active");
+    gridViewbtn.classList.add("view-active");
+    dir1Even.forEach(even => {
+      even.classList.remove("list-view");
+    });
+    card.style.border = null;
+
+    if (vw >= 560) {
+      dir.classList.remove("list-layout-med");
+      dir1.forEach(section => {
+        section.classList.remove("list-col");
+      });
+    }
+  }
 }
