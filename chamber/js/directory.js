12e0bcfd16ab9c6511579614811e248fa306d164
silos-sk <sil21019@byui.edu>
2022-03-12 22:12:54 +0000

    revert aspect ratio

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -37,8 +37,6 @@ function displayCompanies(company) {
   img.setAttribute("src", `${company.logo}`);
   img.setAttribute("alt", `${company.name}`);
   img.setAttribute("loading", "lazy");
-  img.setAttribute("width", "100");
-  img.setAttribute("height", "75");
 
   // Default view (grid)
   function defaultView() {
