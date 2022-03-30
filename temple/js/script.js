bc88ee71264461aa88b31ba928d19e797360ff6d
silos-sk <sil21019@byui.edu>
2022-03-30 22:46:48 +0100

    temple site layout

---
+++ temple/js/script.js
@@ -0,0 +1,47 @@
+let completeDate = new Date();
+const daysName = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const monthName = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+//--- HEADER DATE DISPLAY --- //
+const day = daysName[completeDate.getDay()];
+const todaysDate = completeDate.getDate();
+const month = monthName[completeDate.getMonth()];
+const year = completeDate.getFullYear();
+
+const currentDate = `${day}, ${todaysDate} ${month} ${year}`;
+
+// Footer Date Display
+document.querySelector("#year").textContent = year;
+
+document.getElementById("lu").textContent =
+  " Last Updated: " + document.lastModified;
+
+// Hamburger Nav
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
